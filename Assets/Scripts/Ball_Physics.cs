51cea96fe7c07e5efe12e2d96d55b85059d6dd11
Jean ROMAGNANI <jromagna@e1r12p1.42.fr>
2017-12-22 23:35:38 +0100

    gtrte

--- Assets/Scripts/Ball_Physics.cs
+++ Assets/Scripts/Ball_Physics.cs
@@ -5,25 +5,46 @@ using UnityEngine;
 public class Ball_Physics : MonoBehaviour {
 
 	public Vector3Int startingVec;
-	
+	public int		mass; // la masse que tu decide sur le machin
+
+
 	Vector3Int		pos; // ca cest oukskile
 	Vector3Int		rot; // ca cst comment cest touner
 
+
+
 	Vector3Int		force; // somme des force sur le truc , ajouter la gravite si ya
-	int				mass; // la masse que tu decide sur le machin
 	Vector3Int		acceleration; // force / masse
 	Vector3Int 		velocity; // acceleration * temps
 	Vector3Int		moveAmount; // velocity * temps
-
 	Vector3Int		reflectDir; // vector3.reflect(normal, vector dariver(normalized))
 	
 	// Use this for initialization
 	void Start () {
-		
+		force = startingVec;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-}
+
+	void FixedUpdate()
+	{
+		acceleration = force / mass;
+		velocity = acceleration * Time.fixedDeltaTime;
+		moveAmount = velocity * Time.fixedDeltaTime;
+		transform.position = transform.position + moveAmount;
+	}
+
+	// static void Spe_AddForce(this Vector3Int force, Vector3Int forceToAddDir, Int power)
+	// {
+		
+	// }
+
+	// static void Spe_Normalise(this Vector3Int v)
+	// {
+		
+	// }
+
+} 
